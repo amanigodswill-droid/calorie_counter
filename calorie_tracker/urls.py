ed0de0eb2749a4971c1543b7452b74954b02d786
amanigodswill-droid <amanigodswill@gmail.com>
2026-09-15 11:22:06 +0300

    Build calorie tracking interface with Tailwind CSS

---
+++ calorie_tracker/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.food_list, name='food_list'),
+    path('add/', views.add_food, name='add_food'),
+    path('delete/<int:food_id>/', views.delete_food, name='delete_food'),
+    path('reset/', views.reset_calories, name='reset_calories'),
+]
--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import FoodItemForm
+from .models import FoodItem
+
+def food_list(request):
+    food_items = FoodItem.objects.order_by('-date_added', '-id')
+    total_calories = sum(
+        food_item.calories for food_item in food_items
+    )
+
+    context = {
+        'food_items': food_items,
+        'total_calories': total_calories,
+    }
+
+    return render(request, 'calorie_tracker/food_list.html', context)
+
+
+def add_food(request):
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('food_list')
+    else:
+        form = FoodItemForm()
+    return render(
+        request,
+        'calorie_tracker/add_food.html',
+        {'form': form}
+    )
+
+def delete_food(request, food_id):
+    if request.method == 'POST':
+        food_item = FoodItem.objects.get(id=food_id)
+        food_item.delete()
+    return redirect('food_list')
+
+def reset_calories(request):
+    if request.method == 'POST':
+        FoodItem.objects.all().delete()
+
+    return redirect('food_list')
